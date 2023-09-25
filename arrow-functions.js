a47a88a6a0c4d12764a879f9ac5047287f228e83
rowanam <rowanamaas@gmail.com>
2023-09-25 14:57:07 +0000

    Initial commit

--- arrow-functions.js
+++ arrow-functions.js
@@ -1,14 +1,45 @@
 /**
- * To run this file in Gitpod, use the 
+ * To run this file in Gitpod, use the
  * command node arrow-functions.js in the terminal
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // Code block
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5);
+// console.log(sum);
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // Code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(6, 7);
+console.log(sum);
 
 // Single Line Arrow Function With Parameters
+const addTwoNumbers2 = (a, b) => a + b;
+// const addTwoNumbers3 = (a, b) => (a + b); // also valid
+
+let sum2 = addTwoNumbers2(10, 11);
+console.log(sum2);
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething("Hi!")
+
+const sayHello = () => console.log("Hello")
+sayHello();
 
 // Returning Multiple Lines
+const returnMultipleLines = () => (
+    `<p>
+        This is a multiline string!
+    </p>`
+)
+
+console.log(returnMultipleLines());
